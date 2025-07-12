1e0eb0becc1774de4a36656947150aa1f5851c18
Op0000 <99061102+Op0000@users.noreply.github.com>
2025-07-12 17:12:24 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,27 @@
 // Main App component
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import PostRide from './pages/PostRide.jsx'
+import SearchRides from './pages/SearchRides.jsx'
+
+function App() {
+  return (
+    <div>
+      <nav className="p-4 bg-blue-600 text-white flex justify-between">
+        <Link to="/" className="font-bold text-xl">Ride Along</Link>
+        <div className="space-x-4">
+          <Link to="/post">Post Ride</Link>
+          <Link to="/search">Search</Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<PostRide />} />
+        <Route path="/search" element={<SearchRides />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
