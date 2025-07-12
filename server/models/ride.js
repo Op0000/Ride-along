63aad518220eb349cdb0279396047a1f83bb2d55
Op0000 <99061102+Op0000@users.noreply.github.com>
2025-07-12 16:51:34 +0530

    Create ride.js

---
+++ server/models/ride.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const RideSchema = new mongoose.Schema({
+  driverName: { type: String, required: true },
+  from: { type: String, required: true },
+  to: { type: String, required: true },
+  via: { type: [String] }, // intermediate stops
+  price: { type: Number, required: true },
+  seatsAvailable: { type: Number, required: true },
+  departureTime: { type: Date, required: true },
+}, { timestamps: true })
+
+export default mongoose.model('Ride', RideSchema)
