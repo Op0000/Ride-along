777397ae2db9e9497f1441bbae46858310be2821
Op0000 <99061102+Op0000@users.noreply.github.com>
2025-07-12 17:01:02 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,14 @@
+import express from 'express'
+import cors from 'cors'
 import rideRoutes from './routes/rideRoutes.js'
 
-app.use('/api/rides', rideRoutes)// App configuration
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json()) // Parses JSON bodies
+
+// Routes
+app.use('/api/rides', rideRoutes)
+
+export default app
