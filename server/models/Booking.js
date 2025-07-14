51fe232ab2ceecadd053cc8320adf869b38f00eb
Op0000 <99061102+Op0000@users.noreply.github.com>
2025-07-14 22:17:22 +0530

    Update Booking.js

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -3,8 +3,12 @@ import mongoose from 'mongoose'
 const bookingSchema = new mongoose.Schema({
   rideId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ride', required: true },
   userId: { type: String, required: true }, // Firebase UID
-  seatsBooked: { type: Number, required: true },
-  contactPhone: { type: String }, // optional field
+  seatsBooked: { type: Number, default: 1 },
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
   bookedAt: { type: Date, default: Date.now }
 })
 
