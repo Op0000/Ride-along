3d8213e996ae7ee18f893e23389c97eb2c7ceb67
Op0000 <99061102+Op0000@users.noreply.github.com>
2025-08-05 10:11:58 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -7,6 +7,17 @@ const userSchema = new mongoose.Schema({
   gender: String,
   email: String,
   phone: String,
+
+  driverVerification: {
+    documents: {
+      idProofUrl: { type: String },
+      licenseUrl: { type: String },
+      rcBookUrl: { type: String },
+      profilePhotoUrl: { type: String },
+    },
+    isVerified: { type: Boolean, default: false },
+    submittedAt: { type: Date },
+  }
 }, { timestamps: true })
 
 export default mongoose.model('User', userSchema)
