c6649f7b20412863e89848dd5b4fd87784eee08f
Op0000 <99061102+Op0000@users.noreply.github.com>
2025-12-24 08:45:27 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,12 +10,12 @@ import { getStorage } from 'firebase/storage'
 
 // ✅ Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDi263BYC5vuqljXaEY_WPj_06gZdt9PqY",
-  authDomain: "ride-along-a36bb.firebaseapp.com",
-  projectId: "ride-along-a36bb",
-  storageBucket: "ride-along-a36bb.appspot.com",
-  messagingSenderId: "658827950786",
-  appId: "1:658827950786:web:9c01fe7067e5601cbf995b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 
 // ✅ Initialize Firebase and Auth
